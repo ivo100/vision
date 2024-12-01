4fe1590f384494f05775d7004147607f56b75999
Ivo Stoyanov <ivostoy@me.com>
2024-12-01 10:26:51 -0800

    timm

---
+++ hellotimm/main.py
@@ -0,0 +1,28 @@
+import urllib
+import timm
+from PIL import Image
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+
+import certifi
+print(certifi.where())
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+urllib.request.urlretrieve(url, filename)
+
+# flip-flop on certificates -
+
+model = timm.create_model('efficientnet_b0', pretrained=True)
+model.eval()
+config = resolve_data_config({}, model=model)
+transform = create_transform(**config)
+
+
+img = Image.open(filename).convert('RGB')
+tensor = transform(img).unsqueeze(0) # transform and add batch dimension
+print(tensor)
+
+
