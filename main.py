eb2c72faae3136a4b2922d89f63a7bbe2a66f9d4
ivo100 <ivostoy@gmail.com>
2024-12-12 20:01:01 -0800

    modules

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from mymodule.helper import greet
+from mynats.sync_nats import Nats
 
-def main():
-    print(greet("World"))
 
-if __name__ == "__main__":
-    main()
+def example():
+    print(greet("Ivo"))
+    try:
+        print("Testing NATS")
+        store = Nats()
+        key = "foo"
+        value = "bar"
+        print(f'put {key} -> value {value}')
+        store.put(key, value)
+        val = store.get(key)
+        print(f'get {key} <- value {val}')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == '__main__':
+    example()
